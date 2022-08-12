10471708c4094639c67b569bea92b48f178a0550
Valeria Yanycheva <valeria.yanycheva@gmail.com>
2022-08-12 11:37:25 +0600

    feat: implement settings

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,6 +1,21 @@
+let state = {
+    language: 'en',
+    photoSource: 'GitHub',
+    blocks: {
+        'time': true,
+        'quote': true,
+        'date': true,
+        'greeting': true,
+        'weather': true,
+        'player': true,
+        // 'todo': true,
+    }
+}
+
+
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
-const greeting = document.querySelector('.greeting');
+const greeting = document.querySelector('.greeting-text');
 const n = document.querySelector('.name');
 
 function showTime(){
@@ -46,19 +61,48 @@ function showGreeting(){
 
 // __________________________________SLIDE________________________________________________
 
-let randomNum = getRandomNum();
+let randomNum = getRandomNum(20);
 const slidePrev = document.querySelector('.slide-prev');
 const slideNext = document.querySelector('.slide-next');
+const tags = document.querySelector('.line-tags');
+tags.value = localStorage.getItem('tags');
+let tagsValue = tags.value;
 
-function getRandomNum(){
-    return 1 + Math.floor(Math.random() * 20);
+function getRandomNum(max){
+    return 1 + Math.floor(Math.random() * max);
 }
 
+function disableTags(){
+    tags.disabled = true;
+    tags.previousElementSibling.style.color = `rgb(82, 82, 82)`;
+    tags.value = '';
+}
 
-async function setBg(){
+function enableTags(){
+    tags.disabled = false;
+    tags.previousElementSibling.style.color = `#fff`;
+}
+
+tags.addEventListener('change', function(){
+    tagsValue = tags.value;
+    setBg(state.photoSource);
+});
+
+
+async function setBg(nameSource){
     let link = '';
-    // await getLinkToImageFlickr().then(str => link = str);
-    link = getLinkToImageGH();
+    if(nameSource == 'GitHub'){
+        link = getLinkToImageGH();
+        disableTags();
+    }
+    if(nameSource == 'Flickr API'){
+        await getLinkToImageFlickr().then(str => link = str);
+        enableTags();
+    }
+    if(nameSource == 'Unsplash API'){
+        await getLinkToImageUnsplash().then(str => link = str);
+        enableTags();
+    }
     const img = new Image();
     img.src = link;
 
@@ -67,7 +111,6 @@ async function setBg(){
     }
 }
 
-setBg();
 
 function getSlideNext(){
     if(randomNum == 20){
@@ -87,12 +130,12 @@ function getSlidePrev(){
 
 slidePrev.addEventListener('click', function(){
     getSlidePrev();
-    setBg();
+    setBg(state.photoSource);
 })
 
 slideNext.addEventListener('click', function(){
     getSlideNext();
-    setBg();
+    setBg(state.photoSource);
 })
 
 // ___________________________________PHOTO-SOURCE_________________________________________
@@ -104,8 +147,11 @@ function getLinkToImageGH(){
 }
 
 async function getLinkToImageUnsplash(){
-    const timeOfDay = getTimeOfDay();
-    const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=${timeOfDay}&client_id=9KmeuUBFkvdIrg4bKEhVPxU9NCrOvSdCOiJhDGnY72g`;
+    let query = getTimeOfDay();
+    if(tagsValue !== ''){
+        query = tagsValue;
+    }
+    const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=${query}&client_id=9KmeuUBFkvdIrg4bKEhVPxU9NCrOvSdCOiJhDGnY72g`;
 
     const res = await fetch(url);
     const data = await res.json();
@@ -113,12 +159,15 @@ async function getLinkToImageUnsplash(){
 }
 
 async function getLinkToImageFlickr(){
-    const timeOfDay = getTimeOfDay();
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9fc685eec8b55355bb9f700e914e1ee1&tags=${timeOfDay}&extras=url_l&format=json&nojsoncallback=1`;
+    let query = getTimeOfDay();
+    if(tagsValue !== ''){
+        query = tagsValue;
+    }
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9fc685eec8b55355bb9f700e914e1ee1&tags=${query}&extras=url_l&format=json&nojsoncallback=1`;
 
     const res = await fetch(url);
     const data = await res.json();
-    return data.photos.photo[randomNum].url_l;
+    return data.photos.photo[getRandomNum(data.photos.photo.length)].url_l;
 }
 
 
@@ -210,7 +259,11 @@ const weatherDescription = document.querySelector('.weather-description');
 const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 const city = document.querySelector('.city');
-city.value = localStorage.getItem('city');
+if(localStorage.getItem('city')){
+    city.value = localStorage.getItem('city');
+}else{
+    city.value = 'Minsk';
+}
 const weatherError = document.querySelector('.weather-error');
 
 city.addEventListener('change', getWeather);
@@ -218,7 +271,7 @@ city.addEventListener('change', getWeather);
 async function getWeather(){
     city.value = city.value.trim();
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=3165e81390d2540d9a839afd0b04ac9f&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${state.language}&appid=3165e81390d2540d9a839afd0b04ac9f&units=metric`;
     
     const res = await fetch(url);
     const data = await res.json();
@@ -235,8 +288,13 @@ async function getWeather(){
         weatherIcon.classList.add(`owf-${data.weather[0].id}`);
         temperature.textContent = `${Math.floor(data.main.temp)}°C`;
         weatherDescription.textContent = `${data.weather[0].description}`;
-        wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
-        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        if(state.language == 'en'){
+            wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+            humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        }else if(state.language == 'ru'){
+            wind.textContent = `Скорость ветра: ${Math.floor(data.wind.speed)} m/s`;
+            humidity.textContent = `Влажность воздуха: ${data.main.humidity}%`;
+        }
     }
 }
 
@@ -244,7 +302,7 @@ getWeather();
 
 // ___________________________________________________QUOTES____________________________________________________
 
-const quote = document.querySelector('.quote');
+const quote = document.querySelector('.quote-text');
 const author = document.querySelector('.author');
 const changeQuote = document.querySelector('.change-quote');
 
@@ -284,34 +342,120 @@ document.addEventListener('click', function(event){
     }
 })
 
+function setActiveLanguage(elem, itemsLanguage){
+    for(let item of itemsLanguage){
+        item.classList.remove('_active')
+    }
+    elem.classList.add('_active');
+    state.language = elem.dataset.name;
+    changeLanguage();
+    itemsLanguage[0].textContent = elem.textContent;
+}
+
 listLanguage.addEventListener('click', function(event){
     if(event.target.tagName == 'LI' && !event.target.classList.contains('default')){
-        console.log(event.target.classList.contains('default'));
-        console.log(event.target)
-        for(let item of itemLanguage){
-            item.classList.remove('_active')
-        }
-        event.target.classList.add('_active');
-        itemLanguage[0].textContent = event.target.textContent;
+        setActiveLanguage(event.target, itemLanguage);
     }
 })
+
+
+function setActiveSource(elem, itemsSource){
+    for(let item of itemsSource){
+        item.classList.remove('_active')
+    }
+    elem.classList.add('_active');
+    state.photoSource = `${elem.textContent}`;
+    setBg(state.photoSource);
+    itemsSource[0].textContent = elem.textContent;
+}
 
 listSource.addEventListener('click', function(event){
     if(event.target.tagName == 'LI' && !event.target.classList.contains('default')){
-        for(let item of itemSource){
-            item.classList.remove('_active')
-        }
-        event.target.classList.add('_active');
-        itemSource[0].textContent = event.target.textContent;
+        setActiveSource(event.target, itemSource);
     }
 })
 
+function showHideBlocks(){
+        for(let nameBlock in state.blocks){
+            let block = document.querySelector(`.${nameBlock}`);
+            block.style.transition = 'all ease 0.5s';
+            if(state.blocks[nameBlock] == false){
+                block.style.visibility = 'hidden';
+                block.style.opacity = '0';
+            }
+            if(state.blocks[nameBlock] == true){
+                block.style.visibility = 'visible';
+                block.style.opacity = '1';
+            }
+        }
+}
+
+function setUncheckedInputs(){
+    let inputs = Array.from(document.querySelectorAll('.line-input'));
+    for(let item in state.blocks){
+        if(state.blocks[item] == false){
+            let unchecked = inputs.find(elem => {
+                if(elem.name == item){return elem;}
+            })
+            unchecked.checked = false;
+        }
+    }
+}
+
+showHideBlocks();
+
+setting.addEventListener('click', function(event){
+    if(event.target.classList.contains('line-input')){
+        if(event.target.checked){
+            state.blocks[event.target.name] = true;
+        }
+        if(!event.target.checked){
+            state.blocks[event.target.name] = false;
+        }
+        showHideBlocks();
+    }
+})
+
+function changeLanguage(){
+    getWeather();
+}
+
+function setSettings(){
+    if(localStorage.getItem(state)){
+        state = JSON.parse(localStorage.getItem(state));
+    }
+    
+    let listItemSource = document.querySelectorAll('.list-source .line-item');
+    let elemPhotoSource;
+    for(let i = 1; i < listItemSource.length; i++){
+        if(listItemSource[i].textContent == state.photoSource){
+            elemPhotoSource = listItemSource[i];
+        }
+    }
+    setActiveSource(elemPhotoSource, listItemSource);
+
+    let listItemLanguage = document.querySelectorAll('.list-language .line-item');
+    let elemLanguage;
+    for(let i = 1; i < listItemLanguage.length; i++){
+        if(listItemLanguage[i].dataset.name == state.language){
+            elemLanguage = listItemLanguage[i];
+        }
+    }
+    setActiveLanguage(elemLanguage, listItemLanguage);
+
+    setUncheckedInputs();
+    showHideBlocks();
+}
+
+setSettings();
 
 // ____________________________________________________LOCAL_STORAGE____________________________________________
 
 function setLocalStorage(){
     localStorage.setItem('name', n.value);
     localStorage.setItem('city', city.value);
+    localStorage.setItem(state, JSON.stringify(state));
+    localStorage.setItem('tags', tagsValue);
 }
 window.addEventListener('beforeunload', setLocalStorage);
 
